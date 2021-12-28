f400335fff1cab9df0340edb575c6c5ec0eec5de
Yaazieli <yaazieli@gmail.com>
2021-12-28 00:29:21 -0500

    commit message

--- src/index.js
+++ src/index.js
@@ -1,83 +1,61 @@
 const container = document.querySelector('.colleges');
 
-const renderColleges = async () => {
-  let uri = 'http://localhost:3000/colleges?_sort=likes&_order=desc';
-
-
-  //need to fix, change to then
-  const res = await fetch(uri);
-  const colleges = await res.json()
+//Render colleges in page
+function renderAllColleges() {
+  fetch('http://localhost:3000/colleges?_sort=likes&_order=desc')
+  .then(res => res.json())
+  .then(colleges => colleges.forEach(colleges => renderOneCollege(colleges)))
+}
   
-  let template = '';
-  colleges.forEach(colleges => {
-    template += `
-    <div class="colleges">
+//DOM render function and adding template for colleges listing
+ function renderOneCollege(colleges){
+  let template = document.createElement('li')
+  template.className = 'colleges'
+  template.innerHTML = `
+     <div class="colleges">
     </div>  <h2>${colleges.name}</h2>
       <p><small>${colleges.likes} likes</small></p>
       <p>${colleges.description}</p>
       <img src="${colleges.imageUrl}">
       <button class="likes" id="likes">Likes ❤️</button>
         `
-   })
-
-  container.innerHTML = template;
+ document.querySelector('.colleges').appendChild(template)
 }
 
-//create more Colleges posts
-
-const form = document.querySelector('form');
-
-const createColleges = async (e) => {
-  e.preventDefault();
+//Event Listener 1 (render colleges in the page - need to refresh after new ones added with the below form))
+window.addEventListener('DOMContentLoaded', renderAllColleges); 
 
 
-  const doc = {
-    name: form.name.value,
-    description: form.description.value,
+//Event Listener 2 (add new colleges created with the form on the data)
+document.querySelector('form').addEventListener('submit', handleSubmitColleges);
+
+
+// Event Handler: Create more colleges using the form
+
+function handleSubmitColleges(e) {
+  e.preventDefault()
+  const collegesObj = {
+    name: e.target.name.value,
+    description: e.target.description.value,
     likes: 0,
-    imageUrl: form.imageUrl.value,
+    imageUrl: e.target.imageUrl.value,
   }
+  renderOneCollege(collegesObj)
+  addColleges(collegesObj)
+}
   
-  //need to fix, change to then
-  //await fetch('http://localhost:3000/colleges', {
-   fetch('http://localhost:3000/colleges', {
+//Fetch request - Post Method to add data via the form, using promise to get response from json
+  function addColleges(collegeObj) { 
+  fetch('http://localhost:3000/colleges', {
     method: 'POST',
-    body: JSON.stringify(doc),
-    headers: { 'Content-Type': 'application/json' }
+    headers: { 
+      'Content-Type': 'application/json' 
+    },
+    body: JSON.stringify(collegeObj),
   })
+  .then(res => res.json())
+  console.log(collegeObj)
   }
 
-
-
-  //LIKES
-
-  function likes(e) {
-    e.preventDefault()
-    let more = parseInt(e.target.previousElementSibling.innerText) + 1
-  
-    fetch(`http://localhost:3000/colleges/${e.target.id}`, {
-      method: 'PATCH',
-      body: JSON.stringify(doc),
-      headers: { 'Content-Type': 'application/json' }
-      })
-      .then(res => res.json())
-      .then((likes_obj => {
-       e.target.previousElementSibling.innerText = `${more} likes`;
-      }))
-  
-    }
-  
-
-//Event Listener 3 (add new colleges created with the form on the data)
-form.addEventListener('submit', createColleges);
-
-
-//Event Listener 2 (render colleges in the page after refresh)
-window.addEventListener('DOMContentLoaded', renderColleges); 
-
-
-
-
-  
 
 
